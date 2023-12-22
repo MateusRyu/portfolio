e8301c48d2bdd123e7a9c6d5b15d438d09d98324
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2023-12-22 17:05:13 -0300

    feat: Create navbar

---
+++ components/navbar/MainNavbar.tsx
@@ -0,0 +1,56 @@
+'use client'
+ 
+import { usePathname } from 'next/navigation'
+
+export default function MainNavbar() {
+  const pathname = usePathname();
+  const nav = [
+    {
+      label: "Página inicial",
+      link: "/"
+    },
+    {
+      label: "Aprendizado",
+      link: "/aprendizado"
+    },
+    {
+      label: "Projetos",
+      link: "/projetos"
+    },
+    {
+      label: "Sobre",
+      link: "/sobre"
+    },
+    {
+      label: "Contato",
+      link: "Contato"
+    }
+  ];
+  const navbar = nav.map( (item) => (pathname == item.link?<li key={item.link} className="disabled">
+        <a href="#" className="text-accent">
+          {item.label}
+        </a>
+      </li>:<li key={item.link}> 
+        <a href={item.link} className="text-neutral-content">
+          {item.label}
+        </a>
+      </li>
+  ))
+
+  return (
+    <nav>
+      <div className="navbar bg-neutral">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl text-neutral-content">
+            Ryu
+          </a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            {navbar}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
