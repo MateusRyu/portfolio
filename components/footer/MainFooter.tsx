a14d7b2bc60afbac1931238eecf4d35e20392a0e
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2023-12-22 22:23:26 -0300

    feat: Update footer background

--- components/footer/MainFooter.tsx
+++ components/footer/MainFooter.tsx
@@ -2,7 +2,7 @@ export default function MainFooter() {
   const currentYear = new Date().getFullYear();
   const heart = <span className="text-rose-500">&hearts;</span>;
   return (
-    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+    <footer className="footer footer-center p-4 bg-neutral text-neutral-content">
       <aside>
         <p>
           {currentYear} - Desenvolvido por Mateus Ryu Yamaguchi com {heart}!
