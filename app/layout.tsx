2fe96206b563f92fe91df53c5e6037d832891a74
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-01-04 23:50:01 -0300

    refactor: Update MainNavbar to Header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import MainNavbar from '@/components/navbar/MainNavbar'
+import MainNavbar from '@/components/navbar/Header'
 import MainFooter from '@/components/footer/MainFooter'
 
 const navItems = [
