42679228d7b7bcdac0a5dfd97ffad653159301c4
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-01-05 22:56:36 -0300

    docs: Update Header story

--- components/navbar/Header.stories.ts
+++ components/navbar/Header.stories.ts
@@ -12,7 +12,7 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Mobile: Story = {
+export const Dark: Story = {
   args: {
     brandName: 'Brand Name',
     navItems: [
