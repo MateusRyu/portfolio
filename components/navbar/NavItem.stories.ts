3be679a070f504ee84532444efc977ab472e5d0e
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-01-06 23:13:32 -0300

    docs: Create NavItem story

---
+++ components/navbar/NavItem.stories.ts
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import NavItem from './NavItem';
+import { NavItemProps } from './NavItem';
+
+const meta: Meta = {
+  title: 'Components/navbar/NavItem',
+  component: NavItem 
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    href: '#',
+    active: false,
+    children: 'Default'
+  },
+};
+
+export const Active: Story = {
+  args: {
+    href: '#',
+    active: true,
+    children: 'Active'
+  }
+}
