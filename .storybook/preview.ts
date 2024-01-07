3250eca566e72c99769c5f0730589732384cddd3
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-01-07 17:45:17 -0300

    docs: Add custom screen sizes to preview

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,51 @@ import type { Preview } from "@storybook/react";
 
 import '../app/globals.css';
 
+const customViewports = {
+  xs: {
+    name: 'Small mobile - breakpoint: xs',
+    styles: {
+      width: '320px',
+      height: '568px'
+    }
+  },
+  xsl: {
+    name: 'Small mobile (landscape) - breakpoint: sm',
+    styles: {
+      width: '568px',
+      height: '320px'
+    }
+  },
+  sm: {
+    name: 'Large mobile - breakpoint: sm',
+    styles: {
+      width: '384px',
+      height: '896px',
+    }
+  },
+  sml: {
+    name: 'Large mobile (landscape) - breakpoint: md',
+    styles: {
+      width: '896px',
+      height: '384px'
+    }
+  },
+  md: {
+    name: 'Tablet - breakpoint: md',
+    styles: {
+      width: '768px',
+      height: '1024px',
+    }
+  },
+  lg: {
+    name: 'Tablet (landscape) - breakpoint: lg',
+    styles: {
+      width: '1024px',
+      height: '768px'
+    }
+  },
+}
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
@@ -11,6 +56,7 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    viewport: { viewports: customViewports },
   },
 };
 
