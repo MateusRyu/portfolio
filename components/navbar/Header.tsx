76591b92c4fb820bb6c3b50432661379f1f250a7
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-02-16 00:28:31 -0300

    fix: update active link in the navbar

--- components/navbar/Header.tsx
+++ components/navbar/Header.tsx
@@ -1,15 +1,15 @@
-'use client'
-import {useState} from 'react';
-import { usePathname } from 'next/navigation';
+"use client";
+import { useState } from "react";
+import { usePathname } from "next/navigation";
 
-import Close from '../icons/Close';
-import Hamburger from '../icons/Hamburger';
-import NavItem from './NavItem';
+import Close from "../icons/Close";
+import Hamburger from "../icons/Hamburger";
+import NavItem from "./NavItem";
 
 export type Item = {
   label: string;
   link: string;
-  active?: boolean
+  active?: boolean;
 };
 
 export type HeaderProps = {
@@ -25,7 +25,7 @@ export default function Header({ brandName, navItems }: HeaderProps) {
   const pathname = usePathname();
 
   for (let i = 0; i < navItems.length; i++) {
-    if (navItems[i].active || pathname === navItems[i].link) {
+    if (pathname === navItems[i].link) {
       navItems[i].active = true;
     } else {
       navItems[i].active = false;
@@ -36,27 +36,31 @@ export default function Header({ brandName, navItems }: HeaderProps) {
     <NavItem key={item.link} href={item.link} active={item.active}>
       {item.label}
     </NavItem>
-  )); 
+  ));
 
   return (
     <header className="bg-neutral py-0 px-0">
       <nav className="w-full flex flex-row items-center justify-between px-4 md:px-8 lg:px-12">
         <a className="text-3xl text-neutral-content" href="/">
-          { brandName }
+          {brandName}
         </a>
-        <ul className="hidden md:flex flex-row gap-4 px-1">
-          {navbar}
-        </ul>
+        <ul className="hidden md:flex flex-row gap-4 px-1">{navbar}</ul>
         <button className="md:hidden btn btn-ghost" onClick={toggleMenu}>
-          <Hamburger color="white" className={isClose?"block":"hidden"} />
-          <Close color="white" className={isClose?"hidden":"block"} />
+          <Hamburger color="white" className={isClose ? "block" : "hidden"} />
+          <Close color="white" className={isClose ? "hidden" : "block"} />
         </button>
       </nav>
       <nav aria-label="mobile">
-        <ul className={isClose?"hidden":"flex flex-col md:hidden border-b-2 border-primary"}>
+        <ul
+          className={
+            isClose
+              ? "hidden"
+              : "flex flex-col md:hidden border-b-2 border-primary"
+          }
+        >
           {navbar}
         </ul>
       </nav>
     </header>
-  )
+  );
 }
