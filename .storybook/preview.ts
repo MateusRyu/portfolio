75e20065de7fc6b4040140c33f409ab39ead72e8
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-03-01 23:49:35 -0300

    chore: normalize code style

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,53 +1,60 @@
 import type { Preview } from "@storybook/react";
 
-import '../app/globals.css';
+import "../app/globals.css";
 
 const customViewports = {
   xs: {
-    name: 'Small mobile - breakpoint: xs',
+    name: "Small mobile - breakpoint: xs",
     styles: {
-      width: '320px',
-      height: '568px'
-    }
+      width: "320px",
+      height: "568px",
+    },
   },
   xsl: {
-    name: 'Small mobile (landscape) - breakpoint: sm',
+    name: "Small mobile (landscape) - breakpoint: sm",
     styles: {
-      width: '568px',
-      height: '320px'
-    }
+      width: "568px",
+      height: "320px",
+    },
   },
   sm: {
-    name: 'Large mobile - breakpoint: sm',
+    name: "Large mobile - breakpoint: sm",
     styles: {
-      width: '384px',
-      height: '896px',
-    }
+      width: "384px",
+      height: "896px",
+    },
   },
   sml: {
-    name: 'Large mobile (landscape) - breakpoint: md',
+    name: "Large mobile (landscape) - breakpoint: md",
     styles: {
-      width: '896px',
-      height: '384px'
-    }
+      width: "896px",
+      height: "384px",
+    },
   },
   md: {
-    name: 'Tablet - breakpoint: md',
+    name: "Tablet - breakpoint: md",
     styles: {
-      width: '768px',
-      height: '1024px',
-    }
+      width: "768px",
+      height: "1024px",
+    },
   },
   lg: {
-    name: 'Tablet (landscape) - breakpoint: lg',
+    name: "Tablet (landscape) - breakpoint: lg",
     styles: {
-      width: '1024px',
-      height: '768px'
-    }
+      width: "1024px",
+      height: "768px",
+    },
   },
-}
+};
 
 const preview: Preview = {
+  // decorators: [
+  //   (Story) => (
+  //     <div className={`${Primary.variable}`}>
+  //       <Story />
+  //     </div>
+  //   ),
+  // ],
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
