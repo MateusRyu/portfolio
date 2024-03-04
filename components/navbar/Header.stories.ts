80773d86e992c0a478c78ff20bf219bc745f789a
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-03-03 22:06:57 -0300

    chore: fix style

--- components/navbar/Header.stories.ts
+++ components/navbar/Header.stories.ts
@@ -1,11 +1,11 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-import Header from './Header';
-import { HeaderProps, Item } from './Header';
+import Header from "./Header";
+import { HeaderProps, Item } from "./Header";
 
 const meta: Meta = {
-  title: 'Components/navbar/Header',
-  component: Header
+  title: "Components/navbar/Header",
+  component: Header,
 };
 
 export default meta;
@@ -13,23 +13,23 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 const defaultArgs = {
-  brandName: 'Brand Name',
+  brandName: "Brand Name",
   navItems: [
-    {label: 'Active', link: "#0", active: true},
-    {label: 'link 1', link: '#1', active: false},
-    {label: 'link 2', link: '#2', active: false}
-  ]
-}
+    { label: "Active", link: "#0", active: true },
+    { label: "link 1", link: "#1", active: false },
+    { label: "link 2", link: "#2", active: false },
+  ],
+};
 
 export const Default: Story = {
-  args: defaultArgs
-}
+  args: defaultArgs,
+};
 
 export const Mobile: Story = {
   args: defaultArgs,
   parameters: {
     viewport: {
-      defaultViewport: 'sm'
-    }
-  }
+      defaultViewport: "sm",
+    },
+  },
 };
