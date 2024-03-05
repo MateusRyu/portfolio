83c50ea1c9e220254fe979d2d19ff142f9cb58a1
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-03-05 01:53:18 -0300

    chore: improves code style

--- components/typography/headings/Heading.tsx
+++ components/typography/headings/Heading.tsx
@@ -4,20 +4,14 @@ export type GenericHeadingProps = HeadingProps & {
   level: "1" | "2" | "3" | "4";
 };
 
-export default function Heading({
-  level,
-  children,
-  className,
-  id,
-}: GenericHeadingProps) {
+function Heading({ level, children, className, id }: GenericHeadingProps) {
   const Tag = `h${level}` as keyof JSX.IntrinsicElements;
+  const style = `font-primary text-h${level} ${className || ""}`;
 
   return (
-    <Tag
-      className={`font-primary text-h${level} ${className || ""}`}
-      {...(id && { id })}
-    >
+    <Tag className={style} {...(id && { id })} >
       {children}
     </Tag>
   );
 }
+export default Heading;
