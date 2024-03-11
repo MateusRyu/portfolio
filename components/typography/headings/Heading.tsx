ba5867331bde0f36a9c3e2c48b5fd1bd15ac0b17
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-03-10 21:05:57 -0300

    refactor: Move font size and line height config from tailwind config to the heading component

--- components/typography/headings/Heading.tsx
+++ components/typography/headings/Heading.tsx
@@ -4,12 +4,30 @@ export type GenericHeadingProps = HeadingProps & {
   level: "1" | "2" | "3" | "4";
 };
 
+export type FontSizeType = {
+  [key in "h1" | "h2" | "h3" | "h4"]: {
+    fontSize: string;
+    lineHeight: string;
+  };
+};
+
+export const fontSize: FontSizeType = {
+  h1: { fontSize: "4.25rem", lineHeight: "4.5rem" },
+  h2: { fontSize: "2.625rem", lineHeight: "3rem" },
+  h3: { fontSize: "1.75rem", lineHeight: "3rem" },
+  h4: { fontSize: "1.25rem", lineHeight: "1.5rem" },
+};
+
 function Heading({ level, children, className, id }: GenericHeadingProps) {
   const Tag = `h${level}` as keyof JSX.IntrinsicElements;
-  const style = `font-primary text-h${level} ${className || ""}`;
+  const fontSizeStyle = `text-[${fontSize[`h${level}`]["fontSize"]}]`;
+  const lineHeight = `leading-[${fontSize[`h${level}`]["lineHeight"]}]`;
+  const style = `font-primary ${fontSizeStyle} ${lineHeight} ${
+    className || ""
+  }`;
 
   return (
-    <Tag className={style} {...(id && { id })} >
+    <Tag className={style} {...(id && { id })}>
       {children}
     </Tag>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,10 +13,6 @@ const config: Config = {
         secondary: ["var(--font-secondary)"],
       },
       fontSize: {
-        h1: ["4.25rem", "4.5rem"],
-        h2: ["2.625rem", "3rem"],
-        h3: ["1.75rem", "3rem"],
-        h4: ["1.25rem", "1.5rem"],
         p: ["1rem", "1.5rem"],
         small: ["0.75rem", "1.5rem"],
       },
